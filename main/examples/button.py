903a1737b6ecd0396fd99a9e895040e5ce77977a
Elias Wachmann <elias.wachmann@gmail.com>
2021-01-04 22:24:40 +0100

    neighbor counting works

--- main/examples/button.py
+++ main/examples/button.py
@@ -2,19 +2,12 @@ from tkinter import *
 root = Tk()  # window initialises
 
 
-def whenclicked():
-    button["bg"] = "yellow"
-    try:
-        lablet.pack_forget()
-    except:
-        lablet = Label(root, bg="green", padx=50,
-                       pady=50).grid(row=1, column=1)
+root.geometry('600x600')
 
 
 button = Button(root, text="hallo", state=DISABLED)  # to disable
-button2 = Button(root, text="padding", padx=50, pady=50,
-                 command=whenclicked, bg="#7CFC00")  # functions without () only pass name
-
+button2 = Button(root, text="padding", padx=50, pady=50, bg="#7CFC00")  # functions without () only pass name
+button.place(x=10,y=23)
 
 # button.pack()
 button2.grid(row=1, column=1)
--- main/main.py
+++ main/main.py
@@ -1,22 +1,25 @@
 from tkinter import *
+import time
 
 
 class CustomButton(object):
     """
     int row,col Coordinates
     bool state state alive or dead
-    int neighbors count of neighbors 
+    int neighbors count of neighbors
     """
 
     def __init__(self, row, col, state, neighbors, Gameboard):
         self.row = row
         self.col = col
+        self.index = row*Gameboard.width+col
         self.state = state
         self.neighbors = neighbors
         self.Gameboard = Gameboard
         # setup button
-        self.button = Button(self.Gameboard.root, width=10, height=5, bg="black",
-                             command=self.Buttonclicked,)
+        self.button = Button(master=self.Gameboard.FrameGrid, width=2, height=1,
+                             bg="black",
+                             command=self.Buttonclicked)
         self.button.grid(row=row, column=col)
 
     def ChangeButtonColor(self):
@@ -32,7 +35,6 @@ class CustomButton(object):
         else:
             self.ChangeButtonColor()
             self.state = 1
-        print("state: {}".format(self.state))
 
 
 class Gameboard(object):
@@ -42,17 +44,61 @@ class Gameboard(object):
 
     def __init__(self, height, width):
         self.root = Tk()
+        self.root.geometry('600x600')  # geometry setup
+        # top menu
+        self.FrameTopNavigation = Frame(master=self.root)
+        self.FrameTopNavigation.place(x=0, y=0, width=600, height=50)
+        # button grid
+        self.FrameGrid = Frame(master=self.root)
+        self.FrameGrid.place(x=0, y=55, width=600, height=540)
+
         self.height = height
         self.width = width
         self.buttons = []
+        self.run = Button(master=self.FrameTopNavigation,
+                          width=20, height=5, bg="white", command=self.runSim,
+                          text="run", activebackground="green")
+        self.run.place(x=0, y=0, width=250, height=50)
 
     def buildGrid(self):
         for i in range(self.height):
             for j in range(self.width):
+                # +1 row for other buttons up top
                 self.buttons.append(CustomButton(i, j, 0, 0, self))
+
+    def runSim(self):
+        self.run.config(state=DISABLED)  # deactivate start
+        print("run sim")
+        dirtySim(self, self.height, self.width)
+
+        # todo kill all buttons start new
+        self.run.config(state=ACTIVE)
+
+
+def dirtySim(Gb, height, width):
+    cellcount = height*width
+    for cellrow in range(height):  # check if cell should live or die
+        for cellcol in range(width):
+            countNeighbors(Gb, Gb.buttons[cellrow*width+cellcol])
+
+
+def countNeighbors(Gb, Cell):
+    Cellindex = Cell.index
+    for rowrelative in range(-1, 2):  # iterate over neighbors
+        for colrelative in range(-1, 2):
+            if(rowrelative == colrelative and colrelative == 0):  # dont count oneself
+                continue
+            try:
+                # count alive neighbors
+                if(Gb.buttons[Cellindex+colrelative+(rowrelative*Gb.width)].state == 1):
+                    Gb.buttons[Cellindex].neighbors += 1
+            except:
+                continue
+
+    print("Cell: r: {} c: {} state: {}, Neighbors {}".format(Cell.row, Cell.col,
+                                                             "LIFE" if Cell.state == 1 else "XXXX", Cell.neighbors))
 
 
 board = Gameboard(3, 3)
-
 board.buildGrid()
 board.root.mainloop()
